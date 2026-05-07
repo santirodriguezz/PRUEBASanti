46cf862d78496d9f5f8127252d42f425c8880998
SheLovesCris <christianhidalgo3954@gmail.com>
2026-05-07 19:10:11 +0200

    ejercicio 7 añadido

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Santiago Rodriguez ALfaro");
         System.out.println("Acabo de hacer cambios Christian Hidalgo Cadena");
+        System.out.println("ejercicio7");
     }
 }
